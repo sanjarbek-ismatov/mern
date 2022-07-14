4bb20d9fd25f286025946e46f73c1e8f0354563e
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-07-14 21:49:25 +0500

    updated

--- src/components/register/main.tsx
+++ src/components/register/main.tsx
@@ -1,9 +1,20 @@
 import { Button, TextField } from "@mui/material";
+import axios from "axios";
 import "../../styles/register/main.scss";
 function Main() {
+  type post = {
+    username: string;
+    email: string;
+    password: string;
+  };
+  function postUser({ username, email, password }: post) {
+    axios.post(
+      `/api/register?username=${username}&mail=${email}&password=${password}`
+    );
+  }
   return (
     <main>
-      <form className="form">
+      <form onSubmit={postUser} className="form">
         <div className="input-form">
           <TextField
             className="input"
