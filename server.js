9cb1f937d7fc1351f105970e86680ba6c2488571
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-07-15 11:55:54 +0500

    updated

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const { log } = require("console");
 const express = require("express");
 const getUser = require("./routes/getUser");
 const signup = require("./routes/singup");
-
+const cors = require("cors");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use("/api/register", signup);
 app.use("/api", getUser);
 const port = process.env.PORT || 4000;
--- src/components/register/main.tsx
+++ src/components/register/main.tsx
@@ -1,41 +1,64 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "../../styles/register/main.scss";
 function Main() {
-  type post = {
-    username: string;
-    email: string;
-    password: string;
-  };
-  function postUser({ username, email, password }: post) {
-    axios.post(
-      `/api/register?username=${username}&mail=${email}&password=${password}`
-    );
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  async function postUser(e: any) {
+    e.preventDefault();
+    await axios
+      .post(`http://localhost:4000/api/register`, {
+        username: username,
+        mail: email,
+        password: password,
+      })
+      .then((data) => localStorage.setItem("user", JSON.stringify(data.data)))
+      .catch((error) => console.log(error));
   }
   return (
     <main>
       <form onSubmit={postUser} className="form">
         <div className="input-form">
           <TextField
+            autoComplete="off"
             className="input"
             label="username"
             variant="outlined"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            value={username}
             required
           ></TextField>
         </div>
         <div className="input-form">
           <TextField
+            autoComplete="off"
             className="input"
             label="email"
             variant="outlined"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
             required
           ></TextField>
         </div>
         <div className="input-form">
           <TextField
+            autoComplete="off"
             className="input"
             label="password"
             variant="outlined"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
             required
           ></TextField>
         </div>
